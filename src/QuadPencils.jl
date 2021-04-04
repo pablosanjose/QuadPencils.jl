ab5ce0f5e46313269a58aec37bb3ef3e6c9cfcec
Pablo San-Jose <lekand@gmail.com>
2021-04-04 20:35:12 +0200

    fixes

--- src/QuadPencils.jl
+++ src/QuadPencils.jl
@@ -74,20 +74,20 @@ Base.size(q::QuadPencil, n...) = size(q.A0, n...)
 # Build second companion linearization, or its Q*C2*V rotation
 ############################################################################################
 struct Linearization{T,M<:AbstractMatrix{T},R}
+    pencil::QuadPencil{T}
     A::M
     B::M
-    V::M
+    P::M
     deflate_tol::R
 end
-
-Linearization(A::M, B::M, V::M; tol = zero(real(T))) where {M} = Linearization(A, B, V, tol)
-Linearization(A::M, B::M) where {M} = Linearization(A, B, one(A), zero(real(T)))
 
 linearize(As...; kw...) = linearize(quadpencil(As...; kw...); kw...)
 
 function linearize(p::QuadPencil{T}; type = 1) where {T}
     A, B = AB(p, type)
-    return Linearization(A, B, one(A), zero(T))
+    P = one(A)
+    tol = default_tol(T)
+    return Linearization(p, A, B, P, tol)
 end
 
 AB(p, type) = type == 1 ? C1(p) : type == 2 ? C2(p) :
@@ -96,25 +96,50 @@ AB(p, type) = type == 1 ? C1(p) : type == 2 ? C2(p) :
 C1(p) = [0I I; p.A0 p.A1], [I 0I; 0I -p.A2]
 C2(p) = [p.A1 -I; p.A0 0I], [-p.A2 0I; 0I -I]
 
+function Base.show(io::IO, l::Linearization{T,M}) where {T,M}
+    print(io, summary(l), "\n",
+"  Matrix size    : $(size(l.A, 1)) × $(size(l.A, 2))
+  Matrix type    : $M
+  Scalings γ, δ  : $(real.(scalingfactors(l)))
+  Deflated       : $(deflationstring(l))")
+end
+
+Base.summary(l::Linearization{T}) where {T} =
+    "Linearization{T}: second companion linearization of quadratic pencil"
+
+deflationstring(l::Linearization) =
+    isdeflated(l) ? "true ($(size(l.P, 1)) -> $(size(l.A, 1)))" : "false"
+
+isdeflated(l) = size(l.P, 1) != size(l.A, 1)
+
+Base.size(l::Linearization, n...) = size(l.A, n...)
+
+scalingfactors(l::Linearization) = scalingfactors(l.pencil.scaling)
+
 ### deflate #############################################################################
 # Deflates zero and infinite eigenvalues in a pencil
 ############################################################################################
-deflate(A::AbstractMatrix, B::AbstractMatrix; kw...) = deflate(pqr(A), B; kw...)
+deflate(A0, A1, A2; kw...) = deflate(linearize(A0, A1, A2; kw...); kw...)
 
-function deflate(qrA::Factorization, B; kw...)
-    Adef0, Bdef0, Pdef0 = deflate_zeros(qrA, B; kw...)
-    Bdef, Adef, Pdef∞ = deflate_infinites(Adef0, Bdef0; kw...)
-    Pdef = Pdef0 * Pdef∞
-    return Linearization(Adef, Bdef, Pdef; kw...)
-
+function deflate(l::Linearization)
+    Adef, Bdef, Pdef = deflate_zeros_infinites(l.A, l.B, l.P; atol = l.deflate_tol)
+    return Linearization(l.pencil, Adef, Bdef, Pdef, l.deflate_tol)
 end
 
+function deflate_zeros_infinites(A, B, P = I; kw...)
+    Adef0, Bdef0, Pdef0 = deflate_zeros(A, B; kw...)
+    Adef, Bdef, Pdef∞ = deflate_infinites(Adef0, Bdef0; kw...)
+    Pdef = P * Pdef0 * Pdef∞
+    return Adef, Bdef, Pdef
+end
+
+deflate_zeros(A::AbstractMatrix, B; kw...) = deflate_zeros(pqr(A), B; kw...)
+
 function deflate_zeros(qrA::Factorization{T}, B::AbstractMatrix; atol = default_tol(T)) where {T}
+    Q´B = getQ´(qrA, B)
     Q´A = getRP´(qrA)
-    #Qdef = getQ´(qrA)
-    Q´B = qrA.Q' * B
     n = size(qrA, 1)
-    r = nonzero_rows(RP´, atol)
+    r = nonzero_rows(Q´A, atol)
     A1 = view(Q´A, 1:r, :)
     B1 = view(Q´B, 1:r, :)
     B2 = view(Q´B, r+1:n, :)
@@ -123,6 +148,7 @@ function deflate_zeros(qrA::Factorization{T}, B::AbstractMatrix; atol = default_
     Pdef = view(P, :, 1:r)
     Adef = A1 * Pdef
     Bdef = B1 * Pdef
+    # Qdef = Matrix(I, r, n) * getQ´(qrA)
     return Adef, Bdef, Pdef
 end
 
@@ -136,29 +162,22 @@ default_tol(::Type{T}) where {T} = sqrt(eps(real(T)))
 ### Tools ##################################################################################
 
 pqr(a::SparseMatrixCSC) = qr(a)
+pqr(a::Adjoint) = qr(copy(a))
 pqr(a) = qr(a, Val(true))
-
-# getQ(qr::Factorization) = qr.Q * Idense(size(qr, 1))
-# getQ(qr::SuiteSparse.SPQR.QRSparse) =
-#     Isparse(size(qr, 1), :, qr.prow) * sparse(qr.Q * Idense(size(qr, 1)))
 
 getQ_rev(qr::Factorization) = qr.Q * Idense(size(qr, 1), reverse(1:size(qr,1)))
 getQ_rev(qr::SuiteSparse.SPQR.QRSparse) =
     Isparse(size(qr, 1), :, qr.prow) * sparse(qr.Q * Idense(size(qr, 1), reverse(1:size(qr,1))))
 
 getQ´(qr::Factorization) = qr.Q' * Idense(size(qr, 1))
+getQ´(qr::Factorization, B) = qr.Q' * B
 getQ´(qr::SuiteSparse.SPQR.QRSparse) =
-    sparse((qr.Q * Idense(size(qr, 1)))') * Isparse(size(qr,1), qr.prow, :)
-
-# getQ´_rev(qr::Factorization) = Idense(size(qr, 1), reverse(1:size(qr,1))) * qr.Q'
-# getQ´_rev(qr::SuiteSparse.SPQR.QRSparse) =
-#     sparse((qr.Q * Idense(size(qr, 1), reverse(1:size(qr,1))))') * Isparse(size(qr,1), qr.prow, :)
+    sparse(qr.Q' * Idense(size(qr, 1))) * Isparse(size(qr,1), qr.prow, :)
+getQ´(qr::SuiteSparse.SPQR.QRSparse, B) =
+    qr.Q' * Matrix(B)[qr.prow, :]
 
 getRP´(qr::Factorization) = qr.R * qr.P'
 getRP´(qr::SuiteSparse.SPQR.QRSparse) = qr.R * Isparse(size(qr, 2), qr.pcol, :)
-
-# getPR´(qr::Factorization) = qr.P * qr.R'
-# getPR´(qr::SuiteSparse.SPQR.QRSparse) = Isparse(size(qr, 2), :, qr.pcol) * qr.R'
 
 Idense(n) = Matrix(I, n, n)
 
@@ -215,15 +234,15 @@ function nonzero_rows(m::AbstractMatrix{T}, atol = sqrt(eps(real(T)))) where {T}
     return n
 end
 
-# function chop!(A::AbstractArray{T}, atol = sqrt(eps(real(T)))) where {T}
-#     for (i, a) in enumerate(A)
-#         if abs(a) < atol
-#             A[i] = zero(T)
-#         elseif abs(a) > 1/atol || isnan(a)
-#             A[i] = T(Inf)
-#         end
-#     end
-#     return A
-# end
+function chop!(A::AbstractArray{T}, atol = sqrt(eps(real(T)))) where {T}
+    for (i, a) in enumerate(A)
+        if abs(a) < atol
+            A[i] = zero(T)
+        elseif abs(a) > 1/atol || isnan(a)
+            A[i] = T(Inf)
+        end
+    end
+    return A
+end
 
 end
